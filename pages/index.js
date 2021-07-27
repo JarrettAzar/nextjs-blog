8dd879b1cbe1f9ec3b3827c1bf9b35b4a1fae9a0
JarrettAzar <jlock98@gmail.com>
2021-07-27 14:38:04 -0400

    sync

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         <section className={utilStyles.headingMd}>
             <center>
               <p>Computer Science and Photography graduate looking for full time opportunities and freelance work.</p>
-              <Link href="/about"><a>about</a></Link> | <a href="https://github.com/JarrettAzar">github</a> | <a href="https://linkedin.com/in/JarrettAzar">linkedin</a> | <a href="mailto:jarrett.b.azar@gmail.com">contact</a>
+              <a href="https://drive.google.com/file/d/1e3MxFmscbvcHE4r_Z1uyKAlS1mte9ga7/view?usp=sharing" target="_blank">resume</a> | <a href="https://github.com/JarrettAzar">github</a> | <a href="https://linkedin.com/in/JarrettAzar">linkedin</a> | <a href="mailto:jarrett.b.azar@gmail.com">contact</a>
             </center>
         </section>
     </Layout>
